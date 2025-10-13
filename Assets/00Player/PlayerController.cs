c2f6e06164b1910397a8b99129b95f7882e309d8
mjmin99 <mjmin99@naver.com>
2025-10-13 17:14:30 +0900

    [Feat] 플레이어 클릭 이동 기능 구현
    - 가장 간단한 플레이어 이동 구현
    - UniTask 추가

---
+++ Assets/00Player/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 targetPos;
+    private bool isMoving;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        HandleMouseClick();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isMoving)
+            MoveToTarget();
+    }
+
+    void HandleMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0)) // 좌클릭
+        {
+            Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorld.z = 0;
+            targetPos = mouseWorld;
+            isMoving = true;
+        }
+    }
+
+    void MoveToTarget()
+    {
+        Vector2 direction = (targetPos - rb.position);
+        if (direction.magnitude < 0.05f)
+        {
+            isMoving = false;
+            rb.linearVelocity = Vector2.zero;
+            return;
+        }
+
+        rb.MovePosition(rb.position + direction.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
